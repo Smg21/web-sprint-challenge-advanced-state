32ba530456373bd4c863e6f9c0471a382760573c
Sabrina <Smg21022@gmail.com>
2023-08-24 17:34:16 -0700

    answer undefined

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import { connect, useDispatch } from 'react-redux'; // Import useDispatch
+import { connect, useDispatch } from 'react-redux'; 
 import { createQuiz } from '../state/action-creators';
 
 function Form(props) {
-  const dispatch = useDispatch(); // Get the dispatch function
+  const dispatch = useDispatch(); 
 
   const [formData, setFormData] = useState({
     question: '',
@@ -26,7 +26,7 @@ function Form(props) {
         false_answer_text: formData.falseAnswer,
       };
 
-      await dispatch(createQuiz(quizPayload)); // Use dispatch here
+      await dispatch(createQuiz(quizPayload));
 
       setFormData({
         question: '',
@@ -88,95 +88,5 @@ function Form(props) {
   );
 }
 
-export default connect(null)(Form); // No need to pass action creator now
+export default connect(null)(Form); 
 
-
-// import React, { useState } from 'react';
-// import { connect } from 'react-redux'; // Make sure you have the right version of 'react-redux'
-// import { createQuiz } from '../state/action-creators';
-
-// function Form(props) {
-//   const [formData, setFormData] = useState({
-//     question: '',
-//     trueAnswer: '',
-//     falseAnswer: '',
-//   });
-
-//   const handleInputChange = (event) => {
-//     const { name, value } = event.target;
-//     setFormData((prevData) => ({ ...prevData, [name]: value }));
-//   };
-
-//   const handleSubmit = async (event) => {
-//     event.preventDefault();
-
-//     try {
-//       const quizPayload = {
-//         question_text: formData.question,
-//         true_answer_text: formData.trueAnswer,
-//         false_answer_text: formData.falseAnswer,
-//       };
-
-//       await props.createQuiz(quizPayload);
-
-//       setFormData({
-//         question: '',
-//         trueAnswer: '',
-//         falseAnswer: '',
-//       });
-//     } catch (error) {
-//       console.error('Error creating quiz:', error);
-//     }
-//   };
-
-//   const isFormValid =
-//     formData.question.trim().length > 0 &&
-//     formData.trueAnswer.trim().length > 0 &&
-//     formData.falseAnswer.trim().length > 0;
-
-//   return (
-//     <div>
-//       <h2>Create New Quiz</h2>
-//       <form onSubmit={handleSubmit}>
-//         <div>
-//           <label>
-//             Question:
-//             <input
-//               type="text"
-//               name="question"
-//               value={formData.question}
-//               onChange={handleInputChange}
-//             />
-//           </label>
-//         </div>
-//         <div>
-//           <label>
-//             True Answer:
-//             <input
-//               type="text"
-//               name="trueAnswer"
-//               value={formData.trueAnswer}
-//               onChange={handleInputChange}
-//             />
-//           </label>
-//         </div>
-//         <div>
-//           <label>
-//             False Answer:
-//             <input
-//               type="text"
-//               name="falseAnswer"
-//               value={formData.falseAnswer}
-//               onChange={handleInputChange}
-//             />
-//           </label>
-//         </div>
-//         <button type="submit" disabled={!isFormValid}>
-//           Create Quiz
-//         </button>
-//       </form>
-//     </div>
-//   );
-// }
-
-// export default connect(null, { createQuiz })(Form);
