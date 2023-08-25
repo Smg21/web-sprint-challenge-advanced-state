be5ce8999ba569360dd8032ca5f3129af6c72d40
Sabrina <Smg21022@gmail.com>
2023-08-24 22:14:24 -0700

    code works but tests not passing

--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -1,39 +1,38 @@
-
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux'; 
 import { moveClockwise, moveCounterClockwise } from '../state/action-creators';
 
-function Wheel() {
+const Wheel = () => {
   const dispatch = useDispatch();
-  const wheelPosition = useSelector(state => state.wheel); // Get the wheel position from the Redux store
+  const wheelPosition = useSelector(state => state.wheel) + 1;
 
   const handleMoveClockwise = () => {
     dispatch(moveClockwise());
   };
-
+  
   const handleMoveCounterClockwise = () => {
     dispatch(moveCounterClockwise());
   };
 
+  const cogElements = Array.from({ length: 6 }, (_, index) => (
+    <div
+      key={index}
+      className={`cog ${wheelPosition === index + 1 ? 'active' : ''}`}
+      style={{ '--i': index }}
+    >
+      {wheelPosition === index + 1 ? 'B' : null}
+    </div>
+  ));
+
   return (
     <div id="wrapper">
-      <div id="wheel">
-        {Array.from({ length: 6 }).map((_, index) => (
-          <div
-            key={index}
-            className={`cog ${wheelPosition === index ? 'active' : ''}`}
-            style={{ '--i': index }}
-          >
-            {wheelPosition === index ? 'B' : null}
-          </div>
-        ))}
-      </div>
+      <div id="wheel">{cogElements}</div>
       <div id="keypad">
         <button onClick={handleMoveCounterClockwise}>Counter clockwise</button>
         <button onClick={handleMoveClockwise}>Clockwise</button>
       </div>
     </div>
   );
-}
+};
 
 export default Wheel;
